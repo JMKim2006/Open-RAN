e15871002941a7a1299594d4cde30c148e675a06
JMKim2006 <jmkim@uclab.re.kr>
2026-07-27 02:52:52 +0900

    Disable broken FlexRIC unit tests for experiment build

--- scripts/build-hosted-minimal.sh
+++ scripts/build-hosted-minimal.sh
@@ -6,9 +6,9 @@ mkdir -p "$build"
 cmake -S "$root/vendor/ocudu" -B "$build/ocudu" -G Ninja \
   -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF \
   -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-cmake --build "$build/ocudu" --target gnb --parallel 2
+cmake --build "$build/ocudu" --target gnb --parallel 4
 cmake -S "$root/vendor/flexric" -B "$build/flexric" -G Ninja \
-  -DCMAKE_BUILD_TYPE=Release -DXAPP_DB=SQLITE3_XAPP \
+  -DCMAKE_BUILD_TYPE=Release -DXAPP_DB=SQLITE3_XAPP -DUNIT_TEST=FALSE \
   -DCMAKE_C_COMPILER_LAUNCHER=ccache -DCMAKE_CXX_COMPILER_LAUNCHER=ccache
-cmake --build "$build/flexric" --parallel 2
+cmake --build "$build/flexric" --parallel 4
 sudo cmake --install "$build/flexric"
